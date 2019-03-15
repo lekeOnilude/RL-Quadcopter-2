41205eb02fbe3b6307cd1a3146985dff0d03545b
GBEMILEKE <lekeonilude@gmail.com>
2019-03-15 08:48:09 +0100

    implement ddpg model

---
+++ agents/model.py
@@ -0,0 +1,108 @@
+from keras import layers, models, optimizers
+from keras import backend as K
+
+class Actor:
+    def __init__(self, state_size, action_size, action_low, action_high):
+        self.action_size = action_size
+        self.action_low = action_low
+        self.action_high = action_high
+        self.action_range = self.action_high - self.action_low
+        self.state_size = state_size
+
+        self.build_model()
+
+    def build_model(self):
+
+        #batchnor_state = layers.BatchNormalization(input_shape = (self.state_size,))
+        state = layers.Input(shape=(self.state_size,))#(batchnor)
+
+        net = layers.Dense(units=32, activation='relu')(state)
+        net = layers.BatchNormalization()(net)
+        net = layers.Dropout(0.25)(net)
+        net = layers.Dense(units= 64, activation='relu')(net)
+        net = layers.Dropout(0.25)(net)
+        net = layers.Dense(units= 32, activation= 'relu')(net)
+
+        output = layers.Dense(units= self.action_size, activation='sigmoid')(net)
+        actions = layers.Lambda(lambda x: (x * self.action_range) + self.action_low)\
+            (output)
+        
+        # Create Keras model
+        self.model = models.Model(inputs=state, outputs=actions)
+
+        # Define loss function using action value (Q value) gradients
+        action_gradients = layers.Input(shape=(self.action_size,))
+        loss = K.mean(-action_gradients * actions)
+
+        # Incorporate any additional losses here (e.g. from regularizers)
+
+        # Define optimizer and training function
+        optimizer = optimizers.Adam()
+        updates_op = optimizer.get_updates(params=self.model.trainable_weights, loss=loss)
+        self.train_fn = K.function(
+            inputs=[self.model.input, action_gradients, K.learning_phase()],
+            outputs=[],
+            updates=updates_op)
+
+
+class Critic:
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size):
+        """Initialize parameters and build model.
+
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+
+        # Initialize any other variables here
+
+        self.build_model()
+
+    def build_model(self):
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        # Define input layers
+        states = layers.Input(shape=(self.state_size,), name='states')
+        actions = layers.Input(shape=(self.action_size,), name='actions')
+
+        # Add hidden layer(s) for state pathway
+        net_states = layers.Dense(units=32, activation='relu')(states)
+        net_states = layers.BatchNormalization()(net_states)
+        net_states = layers.Dropout(0.25)(net_states)
+        net_states = layers.Dense(units=64, activation='relu')(net_states)
+
+        # Add hidden layer(s) for action pathway
+        net_actions = layers.Dense(units=32, activation='relu')(actions)
+        net_actions = layers.BatchNormalization()(net_actions)
+        net_actions = layers.Dropout(0.25)(net_actions)
+        net_actions = layers.Dense(units=64, activation='relu')(net_actions)
+
+        # Try different layer sizes, activations, add batch normalization, regularizers, etc.
+
+        # Combine state and action pathways
+        net = layers.Add()([net_states, net_actions])
+        net = layers.Activation('relu')(net)
+
+        # Add more layers to the combined network if needed
+
+        # Add final output layer to prduce action values (Q values)
+        Q_values = layers.Dense(units=1, name='q_values')(net)
+
+        # Create Keras model
+        self.model = models.Model(inputs=[states, actions], outputs=Q_values)
+
+        # Define optimizer and compile model for training with built-in loss function
+        optimizer = optimizers.Adam()
+        self.model.compile(optimizer=optimizer, loss='mse')
+
+        # Compute action gradients (derivative of Q values w.r.t. to actions)
+        action_gradients = K.gradients(Q_values, actions)
+
+        # Define an additional function to fetch action gradients (to be used by actor model)
+        self.get_action_gradients = K.function(
+            inputs=[*self.model.input, K.learning_phase()],
+            outputs=action_gradients)
