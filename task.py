2766a48b20b997c75adbe380025253de1e8ca527
GBEMILEKE <lekeonilude@gmail.com>
2019-03-16 20:17:45 +0100

    updated the reward function

--- task.py
+++ task.py
@@ -18,7 +18,7 @@ class Task():
         self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
         self.action_repeat = 3
 
-        self.state_size = self.action_repeat * 6
+        self.state_size = self.action_repeat * 9
         self.action_low = 0
         self.action_high = 900
         self.action_size = 4
@@ -28,7 +28,14 @@ class Task():
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
-        reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        
+        reward_d = np.tanh(- 0.001*(abs(self.sim.pose[:3] - self.target_pos).sum()))
+        reward_z = np.tanh(0.03 * (self.sim.v[2]))
+        reward_h = np.tanh(- 0.003*(abs(self.sim.pose[2] - self.target_pos[2])))
+        reward_angle =  np.tanh(- 0.03 * (self.sim.angular_v).sum() )
+        reward = (reward_angle + reward_d + reward_h + reward_z) / 4
+        
+        #reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
         return reward
 
     def step(self, rotor_speeds):
@@ -38,12 +45,12 @@ class Task():
         for _ in range(self.action_repeat):
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
             reward += self.get_reward() 
-            pose_all.append(self.sim.pose)
+            pose_all.append(np.concatenate((self.sim.pose,  self.sim.v)))
         next_state = np.concatenate(pose_all)
         return next_state, reward, done
 
     def reset(self):
         """Reset the sim to start a new episode."""
         self.sim.reset()
-        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        state = np.concatenate([np.concatenate((self.sim.pose, self.sim.v))] * self.action_repeat) 
         return state
